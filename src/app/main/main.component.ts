bfa6f3801db31b1473605d4df00c25c360a3022b
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-09-18 09:45:13 +0200

    Ajout de la date dynamique dans le footer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,26 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
-import { Task } from "../tasks/task";
-import { Tasks } from "../tasks/mock.tasks";
-
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
   styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
-  constructor() {}
-  tasks = Tasks; // Tableau des tâches
-  selectedTask?: Task;
+  date = new Date();
 
-  /**
-   * Sélectionner la tâche
-   * @param task : Object
-   */
-  onSelect(task: Task): void {
-    this.selectedTask = task;
-    console.log(task.id);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
